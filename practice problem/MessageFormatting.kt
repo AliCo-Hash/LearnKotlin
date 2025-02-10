04da70da1cdab8ea73ec981412c5d57369a657b3
AliCo-Hash <ali.cocelli@outlook.com>
2025-02-10 15:48:49 +0000

    message formatting

---
+++ practice problem/MessageFormatting.kt
@@ -0,0 +1,15 @@
+/*
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = "$baseSalary + $bonusAmount"
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+*/
+
+fun main() {
+    val baseSalary: Int = 5000
+    val bonusAmount: Int = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
