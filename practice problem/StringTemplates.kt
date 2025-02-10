d6e70c2d0c5f4afcaf88c16fa6a8598c8b01a1ea
AliCo-Hash <ali.cocelli@outlook.com>
2025-02-10 15:42:37 +0000

    string templates problem

---
+++ practice problem/StringTemplates.kt
@@ -0,0 +1,20 @@
+/* Problem
+fun main() {
+    val discountPercentage: Int = 0
+    val offer: String = ""
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+} */
+
+fun main() {
+    var discountPercentage: Int
+    var offer: String
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+}
